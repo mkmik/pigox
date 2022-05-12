9f25b6a47c81027c68d6f6363eb51ad503640a4c
Marko Mikulicic <mmikulicic@gmail.com>
2022-05-13 01:54:48 +0200

    Initial

---
+++ go.mod
@@ -0,0 +1,14 @@
+module github.com/mkmik/piggo
+
+go 1.18
+
+require (
+	github.com/alecthomas/kong v0.5.0
+	github.com/jackc/pgproto3/v2 v2.3.0
+)
+
+require (
+	github.com/jackc/chunkreader/v2 v2.0.0 // indirect
+	github.com/jackc/pgio v1.0.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+)
---
+++ main.go
@@ -0,0 +1,146 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/alecthomas/kong"
+	"github.com/jackc/pgproto3/v2"
+)
+
+type Context struct {
+	*CLI
+}
+
+type CLI struct {
+	ListenAddress string `optional:"" default:"localhost:1234"`
+}
+
+type proxy struct {
+	backend *pgproto3.Backend
+	conn    net.Conn
+}
+
+func newProxy(conn net.Conn) proxy {
+	backend := pgproto3.NewBackend(pgproto3.NewChunkReader(conn), conn)
+
+	return proxy{
+		backend: backend,
+		conn:    conn,
+	}
+}
+
+func (p *proxy) Run() error {
+	defer p.Close()
+
+	err := p.handleStartup()
+	if err != nil {
+		return err
+	}
+
+	for {
+		msg, err := p.backend.Receive()
+		if err != nil {
+			return fmt.Errorf("error receiving message: %w", err)
+		}
+
+		switch msg := msg.(type) {
+		case *pgproto3.Query:
+			log.Println("Got query", msg.String)
+
+			response := []byte("foo")
+
+			buf := (&pgproto3.RowDescription{Fields: []pgproto3.FieldDescription{
+				{
+					Name:                 []byte("fortune"),
+					TableOID:             0,
+					TableAttributeNumber: 0,
+					DataTypeOID:          25,
+					DataTypeSize:         -1,
+					TypeModifier:         -1,
+					Format:               0,
+				},
+			}}).Encode(nil)
+			buf = (&pgproto3.DataRow{Values: [][]byte{response}}).Encode(buf)
+			buf = (&pgproto3.CommandComplete{CommandTag: []byte("SELECT 1")}).Encode(buf)
+			buf = (&pgproto3.ReadyForQuery{TxStatus: 'I'}).Encode(buf)
+			_, err = p.conn.Write(buf)
+			if err != nil {
+				return fmt.Errorf("error writing query response: %w", err)
+			}
+		case *pgproto3.Terminate:
+			log.Println("got terminate message")
+			return nil
+		default:
+			return fmt.Errorf("received message other than Query from client: %#v", msg)
+		}
+	}
+}
+
+func (p *proxy) handleStartup() error {
+	startupMessage, err := p.backend.ReceiveStartupMessage()
+	if err != nil {
+		return fmt.Errorf("error receiving startup message: %w", err)
+	}
+
+	switch startupMessage.(type) {
+	case *pgproto3.StartupMessage:
+		buf := (&pgproto3.AuthenticationOk{}).Encode(nil)
+		buf = (&pgproto3.ReadyForQuery{TxStatus: 'I'}).Encode(buf)
+		_, err = p.conn.Write(buf)
+		if err != nil {
+			return fmt.Errorf("error sending ready for query: %w", err)
+		}
+	case *pgproto3.SSLRequest:
+		_, err = p.conn.Write([]byte("N"))
+		if err != nil {
+			return fmt.Errorf("error sending deny SSL request: %w", err)
+		}
+		return p.handleStartup()
+	default:
+		return fmt.Errorf("unknown startup message: %#v", startupMessage)
+	}
+
+	return nil
+}
+
+func (p *proxy) Close() error {
+	return p.conn.Close()
+}
+
+func (cmd *CLI) Run(cli *Context) error {
+	ln, err := net.Listen("tcp", cmd.ListenAddress)
+	if err != nil {
+		return err
+	}
+	log.Println("Listening on", ln.Addr())
+
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Println("Accepted connection from", conn.RemoteAddr())
+
+		b := newProxy(conn)
+
+		go func() {
+			err := b.Run()
+			if err != nil {
+				log.Println(err)
+			}
+			log.Println("Closed connection from", conn.RemoteAddr())
+		}()
+	}
+
+	return nil
+}
+
+func main() {
+	var cli CLI
+	ctx := kong.Parse(&cli)
+	err := ctx.Run(&Context{CLI: &cli})
+	ctx.FatalIfErrorf(err)
+	//	cli.OnExit()
+}
